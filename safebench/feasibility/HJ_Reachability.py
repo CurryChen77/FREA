fix the rotation problem of plot feasibility code

safebench/feasibility/HJ_Reachability.py
@@ -39,6 +39,7 @@ class HJR:
         self.gamma = config['gamma']
         self.tau = config['tau']
         self.expectile = config['expectile']
+        self.M = config['M']
 
         self.batch_size = config['batch_size']
         self.model_path = os.path.join(config['ROOT_DIR'], config['model_path'], 'min_dis_threshold_' + str(self.min_dis_threshold))
@@ -173,8 +174,8 @@ class HJR:
             # ego_collide = batch['ego_collide']  # if 1.0 means ego collide elif 0.0 means ego not collide
             # h = torch.where(torch.isclose(ego_collide, torch.tensor(1.0), atol=0.01), 20.0, -1.0)
 
-            # h is -1.0 when Ego is safe, else, h is 10
-            h = torch.where(min_dis < float(self.min_dis_threshold), 10.0, -1)
+            # h is -1.0 when Ego is safe, else, h is M
+            h = torch.where(min_dis < float(self.min_dis_threshold), self.M, -1)
 
             del min_dis
 

safebench/feasibility/plot_feasibility.py
@@ -207,10 +207,10 @@ def plot_feasibility_region(ax, agent, ego_obs, ego_speed, spatial_interval=10, 
     for i, Vehicle in enumerate(ego_obs):
         # the x,y in Rectangle in the bottom left corner
         color = 'r' if i == 0 else (0.30, 0.52, 0.74)
-        rectangle = Rectangle((Vehicle[0]-Vehicle[2] / 2, Vehicle[1]-Vehicle[3] / 2), width=Vehicle[2], height=Vehicle[3], angle=np.degrees(Vehicle[4]), rotation_point='center', fill=True, alpha=0.5, color=color)
+        rectangle = Rectangle((Vehicle[0]-Vehicle[2] / 2, Vehicle[1]-Vehicle[3] / 2), width=Vehicle[2], height=Vehicle[3], angle=-np.degrees(Vehicle[4]), rotation_point='center', fill=True, alpha=0.5, color=color)
         angle_rad = Vehicle[4]
         speed = ego_speed if i == 0 else Vehicle[5]
-        direction = [np.cos(angle_rad) * speed, np.sin(angle_rad) * speed]
+        direction = [np.cos(angle_rad) * speed, - np.sin(angle_rad) * speed]
         arrow = Arrow(x=Vehicle[0], y=Vehicle[1], dx=direction[0], dy=direction[1], width=1.5)
         ax.add_patch(rectangle)
         ax.add_patch(arrow)
@@ -290,7 +290,7 @@ def plot_multi_feasibility_region(args):
     ego_obs2 = generate_ego_obs(ego_speed=4, yaw_angle=(-3.14/2, -3.14/2), actor_num=actor_num, x_range=x_range, y_range=y_range, width=width, height=height, speed_range=(1, 5))
     ax3 = plot_feasibility_region(ax3, feasibility_policy, ego_obs2, ego_speed=4, spatial_interval=spatial_interval, actor_num=actor_num)
 
-    ego_obs3 = generate_ego_obs(ego_speed=4, yaw_angle=(3.14/2, 3.14/2), actor_num=actor_num, x_range=x_range, y_range=y_range, width=width, height=height, speed_range=(1, 5))
+    ego_obs3 = generate_ego_obs(ego_speed=4, yaw_angle=(-3.14/2, 3.14/2), actor_num=actor_num, x_range=x_range, y_range=y_range, width=width, height=height, speed_range=(1, 5))
     ax4 = plot_feasibility_region(ax4, feasibility_policy, ego_obs3, ego_speed=4, spatial_interval=spatial_interval, actor_num=actor_num)
 
     for ax in [ax1, ax2, ax3, ax4]:
@@ -324,7 +324,7 @@ if __name__ == '__main__':
     parser.add_argument('--data_filename', type=str, default='merged_data.hdf5')
     parser.add_argument('--mode', '-m', type=str, default='region', choices=['region', 'data'])
     parser.add_argument('--actor_num', type=int, default=3)
-    parser.add_argument('--spatial_interval', type=int, default=32)
+    parser.add_argument('--spatial_interval', type=int, default=64)
     parser.add_argument('--x_range', type=tuple, default=(-22, 22))
     parser.add_argument('--y_range', type=tuple, default=(-7, 7))
     parser.add_argument('--width', type=float, default=4.9)

safebench/gym_carla/envs/carla_env.py
@@ -556,7 +556,7 @@ class CarlaEnv(gym.Env):
                 'simple_state': simple_state.astype(np.float32),
             }
         elif self.agent_obs_type == 'ego_obs':
-            # the ego obs for RL training need the route_info
+            # the ego obs for RL training needs the route_info
             obs = self.scenario_manager.route_scenario.update_ego_info(self.ego_nearby_vehicles, waypoints=self.waypoints, need_route_info=True)
         elif self.agent_obs_type == 'no_obs':
             obs = None
@@ -633,7 +633,7 @@ class CarlaEnv(gym.Env):
         # _, candidates_id = get_CBV_candidates(self.ego_vehicle, self.target_waypoint, self.search_radius, ego_fov=100)
         for CBV_id, CBV in self.CBVs.items():
             if not self.scenario_manager.running:
-                # if the Ego stop or the CBV no longer exists in the CBV candidates, then the CBV is truncated
+                # if the Ego stops or the CBV no longer exists in the CBV candidates, then the CBV is truncated
                 CBVs_truncated[CBV_id] = True
             elif get_distance_across_centers(CBV, self.ego_vehicle) >= self.search_radius:
                 # if the CBV is too far away from the ego vehicle, then no long need the CBV
@@ -660,7 +660,7 @@ class CarlaEnv(gym.Env):
             self.sem_sensor.destroy()
             self.sem_sensor = None
         if self.CBVs_collision_sensor:
-            # remove the collision sensor that have not been destroyed
+            # remove the collision sensor that has not been destroyed
             for sensor in self.CBVs_collision_sensor.values():
                 sensor.stop()
                 sensor.destroy()