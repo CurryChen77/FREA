add feasibility Qs and Vs in infos, and fix the feasibility advantage calculation method in fppo

safebench/carla_runner.py
@@ -14,6 +14,7 @@ import os
 import numpy as np
 import carla
 import pygame
+from safebench.feasibility import FEASIBILITY_LIST
 from tqdm import tqdm
 import os.path as osp
 from torch.utils.tensorboard import SummaryWriter
@@ -55,8 +56,8 @@ class CarlaRunner:
         self.fixed_delta_seconds = scenario_config['fixed_delta_seconds']
         self.CBV_selection = scenario_config['CBV_selection']
         self.scenario_id = scenario_config['scenario_id']
-        # if train feasibility then definitely need feasibility, else, depending on whether scenario agent need feasibility
-        self.use_feasibility = scenario_config['feasibility']
+        # if the scenario agent need feasibility or need feasibility for eval
+        self.use_feasibility = scenario_config['feasibility'] or scenario_config['use_feasibility']
         self.scenario_policy_type = scenario_config['policy_type']
         self.agent_policy_type = agent_config['policy_type']
 
@@ -114,9 +115,6 @@ class CarlaRunner:
         # pass config from agent to scenario
         scenario_config['agent_policy'] = agent_config['policy_type']
         scenario_config['agent_obs_type'] = agent_config['obs_type']
-        if self.use_feasibility:
-            # if the scenario agent needs feasibility, then pass the config in
-            scenario_config['feasibility_config'] = feasibility_config
 
         CarlaDataProvider.set_ego_desired_speed(self.env_params['desired_speed'])
 
@@ -183,9 +181,10 @@ class CarlaRunner:
 
         self.logger.log('>> ' + '-' * 40)
 
-        # define agent policy, scenario policy
+        # define agent policy, scenario policy (feasibility policy)
         self.agent_policy = AGENT_POLICY_LIST[agent_config['policy_type']](agent_config, logger=self.logger)
         self.scenario_policy = SCENARIO_POLICY_LIST[scenario_config['policy_type']](scenario_config, logger=self.logger)
+        self.feasibility_policy = FEASIBILITY_LIST[feasibility_config['type']](feasibility_config, logger=self.logger) if self.use_feasibility else None
 
         if self.save_video:
             assert self.mode == 'eval', "only allow video saving in eval mode"
@@ -249,7 +248,7 @@ class CarlaRunner:
             # sample scenarios in this town (one town could contain multiple scenarios)
             # simulate multiple scenarios in parallel (usually 2 scenarios)
             sampled_scenario_configs, config_lengths = data_loader.sampler()
-            # if config length < num_scenarios, means need to balance to storing placement within buffer
+            # if config length < num_scenarios, needs to balance to storing placement within buffer
             if self.mode == 'train_agent' and self.agent_policy.type == 'onpolicy':
                 buffer.check_scenario_id_for_saving(config_lengths)
             # reset the index counter to create endless loader
@@ -441,7 +440,7 @@ class CarlaRunner:
                 self.world,
                 self.birdeye_render,
                 self.display,
-                self.use_feasibility,
+                self.feasibility_policy,
                 self.agent_state_encoder,
                 self.logger,
             )
@@ -457,6 +456,11 @@ class CarlaRunner:
                 self.scenario_policy.load_model(map_name=self.current_map)
                 self.agent_policy.set_mode('eval')
                 self.scenario_policy.set_mode('eval')
+                if self.use_feasibility:
+                    # loading the feasibility policy model
+                    self.feasibility_policy.load_model(map_name=self.current_map)
+                    self.feasibility_policy.set_mode('eval')
+                    assert self.feasibility_policy.continue_episode != 0, 'The scenario policy need well-trained feasibility network'
                 if self.agent_state_encoder:
                     self.agent_state_encoder.load_ckpt()
                 self.eval(data_loader)
@@ -473,6 +477,11 @@ class CarlaRunner:
                 self.agent_policy.load_model(map_name=self.current_map)
                 self.agent_policy.set_mode('eval')
                 self.scenario_policy.set_mode('train')
+                if self.use_feasibility:
+                    # loading the feasibility policy model
+                    self.feasibility_policy.load_model(map_name=self.current_map)
+                    self.feasibility_policy.set_mode('eval')
+                    assert self.feasibility_policy.continue_episode != 0, 'The scenario policy need well-trained feasibility network'
                 if self.agent_state_encoder:
                     self.agent_state_encoder.load_ckpt()
                 self.train(data_loader, start_episode)

safebench/feasibility/HJ_Reachability.py
@@ -75,53 +75,12 @@ class HJR:
         for p in self.Vh_optimizer.param_groups:
             p['lr'] = lr_now
 
-    # def find_min_Qh(self, n_state):
-    #     """
-    #         Utilize gradient-free optimization methods to traverse the action space and find the minimum Qh value
-    #     """
-    #     def Qh(acceleration, steering, next_state):
-    #         acceleration = torch.from_numpy(acceleration)
-    #         steering = torch.from_numpy(steering)
-    #         safest_action = CUDA(torch.cat((acceleration, steering), dim=1).type(next_state.dtype))
-    #         Qh_value = self.Qh_net(next_state, safest_action)
-    #         # the object is to min the Qh value of each state (equal to min sum of overall Qh)
-    #         return Qh_value.sum().item()
-    #
-    #     parametrization = ng.p.Instrumentation(
-    #         acceleration=ng.p.Array(shape=(self.batch_size, 1)).set_bounds(-3.0, 3.0),
-    #         steering=ng.p.Array(shape=(self.batch_size, 1)).set_bounds(-0.3, 0.3),
-    #         next_state=n_state
-    #     )
-    #
-    #     optimizer = ng.optimizers.NGOpt(parametrization=parametrization, budget=100)
-    #     recommendation = optimizer.minimize(Qh)
-    #
-    #     acc = recommendation.kwargs['acceleration']
-    #     steer = recommendation.kwargs['steering']
-    #     acc = torch.from_numpy(acc)
-    #     steer = torch.from_numpy(steer)
-    #     best_action = CUDA(torch.cat((acc, steer), dim=1).type(n_state.dtype))
-    #     min_Qs = self.Qh_net(n_state, best_action)
-    #     return min_Qs
-
-    # @staticmethod
-    # def process_infos(infos):
-    #     ego_obs = [info['ego_obs'] for info in infos]
-    #     ego_obs = np.stack(ego_obs, axis=0)
-    #     return ego_obs.reshape(ego_obs.shape[0], -1)
-    #
-    # def get_feasibility_value(self, infos):
-    #     state = self.process_infos(infos)
-    #     state = CUDA(torch.FloatTensor(state))
-    #     feasibility_value = self.Vh_net(state)
-    #     return feasibility_value
-
-    def get_feasibility_V(self, state):
+    def get_feasibility_Vs(self, state):
         state = state.reshape(state.shape[0], -1)
         feasibility_value = self.Vh_net(state)
         return feasibility_value
 
-    def get_feasibility_Q(self, state, action):
+    def get_feasibility_Qs(self, state, action):
         state = state.reshape(state.shape[0], -1)
         feasibility_Q = self.Qh_target_net(state, action)
         return feasibility_Q

safebench/gym_carla/buffer.py
@@ -310,10 +310,9 @@ class RolloutBuffer:
             self.buffer_dones = np.zeros(self.buffer_capacity, dtype=np.float32)
             self.buffer_terminated = np.zeros((self.buffer_capacity), dtype=np.float32)
             if self.use_feasibility:
-                self.buffer_feasibility_action = np.zeros((self.buffer_capacity, self.feasibility_action_dim), dtype=np.float32)
-                self.buffer_feasibility_obs = np.zeros((self.buffer_capacity, *self.feasibility_obs_shape), dtype=np.float32)
-                self.buffer_feasibility_next_obs = np.zeros((self.buffer_capacity, *self.feasibility_obs_shape), dtype=np.float32)
-                self.temp_buffer.update({'feasibility_actions': {}, 'feasibility_obs': {}, 'feasibility_next_obs': {}})
+                self.buffer_feasibility_Qs = np.zeros((self.buffer_capacity), dtype=np.float32)
+                self.buffer_feasibility_Vs = np.zeros((self.buffer_capacity), dtype=np.float32)
+                self.temp_buffer.update({'feasibility_Qs': {}, 'feasibility_Vs': {}})
         elif self.mode == 'train_agent':
             self.agent_pos = [0] * self.num_scenario
             self.agent_full = [False] * self.num_scenario
@@ -341,13 +340,12 @@ class RolloutBuffer:
         # the processed data for normal scenario training
         processed_actions, processed_log_probs, processed_obs, processed_next_obs, processed_rewards, processed_dones, processed_terminated = [], [], [], [], [], [], []
         # the processed data for feasibility
-        processed_feasibility_actions, processed_feasibility_obs, processed_feasibility_next_obs = [], [], []
-        all_agent_actions = data_list[0]  # agent actions
+        processed_feasibility_Qs, processed_feasibility_Vs = [], []
         all_scenario_actions, all_scenario_log_probs = data_list[1]  # scenario actions are in the datalist[1]
 
         assert len(all_scenario_actions) == len(additional_dict[0]) == len(additional_dict[1]), "the length of info and next_info should be the same"
         # Traverse all the step data in all scenarios
-        for agent_action, actions, log_probs, infos, next_infos in zip(all_agent_actions, all_scenario_actions, all_scenario_log_probs, additional_dict[0], additional_dict[1]):
+        for actions, log_probs, infos, next_infos in zip(all_scenario_actions, all_scenario_log_probs, additional_dict[0], additional_dict[1]):
             assert len(actions) == len(next_infos['CBVs_obs']) == len(infos['CBVs_obs']) \
                 == len(next_infos['CBVs_reward']) == len(next_infos['CBVs_truncated']), "length of the trajectory should be the same"
             # Traverse all the CBVs in one scenario
@@ -362,9 +360,8 @@ class RolloutBuffer:
                     self.temp_buffer['dones'][CBV_id] = []
                     self.temp_buffer['terminated'][CBV_id] = []
                     if self.use_feasibility:
-                        self.temp_buffer['feasibility_actions'][CBV_id] = []
-                        self.temp_buffer['feasibility_obs'][CBV_id] = []
-                        self.temp_buffer['feasibility_next_obs'][CBV_id] = []
+                        self.temp_buffer['feasibility_Qs'][CBV_id] = []
+                        self.temp_buffer['feasibility_Vs'][CBV_id] = []
 
                 # add one-step trajectory in to the corresponding CBV dict
                 self.temp_buffer['actions'][CBV_id].append(actions[CBV_id])
@@ -374,9 +371,8 @@ class RolloutBuffer:
                 self.temp_buffer['rewards'][CBV_id].append(next_infos['CBVs_reward'][CBV_id])
                 if self.use_feasibility:
                     # for all the CBV in the scenario, all the feasibility action, feasibility obs, feasibility next obs should be the same
-                    self.temp_buffer['feasibility_actions'][CBV_id].append(agent_action)
-                    self.temp_buffer['feasibility_obs'][CBV_id].append(infos[self.feasibility_policy_type])
-                    self.temp_buffer['feasibility_next_obs'][CBV_id].append(next_infos[self.feasibility_policy_type])
+                    self.temp_buffer['feasibility_Qs'][CBV_id].append(next_infos['feasibility_Q'])
+                    self.temp_buffer['feasibility_Vs'][CBV_id].append(next_infos['feasibility_V'])
 
                 if next_infos['CBVs_terminated'][CBV_id] or next_infos['CBVs_truncated'][CBV_id]:
                     self.temp_buffer['dones'][CBV_id].append(True)
@@ -390,15 +386,14 @@ class RolloutBuffer:
                     processed_dones.extend(self.temp_buffer['dones'].pop(CBV_id))
                     processed_terminated.extend(self.temp_buffer['terminated'].pop(CBV_id))
                     if self.use_feasibility:
-                        processed_feasibility_actions.extend(self.temp_buffer['feasibility_actions'].pop(CBV_id))
-                        processed_feasibility_obs.extend(self.temp_buffer['feasibility_obs'].pop(CBV_id))
-                        processed_feasibility_next_obs.extend(self.temp_buffer['feasibility_next_obs'].pop(CBV_id))
+                        processed_feasibility_Qs.extend(self.temp_buffer['feasibility_Qs'].pop(CBV_id))
+                        processed_feasibility_Vs.extend(self.temp_buffer['feasibility_Vs'].pop(CBV_id))
                 else:
                     self.temp_buffer['dones'][CBV_id].append(False)
                     self.temp_buffer['terminated'][CBV_id].append(False)
 
         return processed_actions, processed_log_probs, processed_obs, processed_next_obs, processed_rewards, processed_dones, processed_terminated, \
-            processed_feasibility_actions, processed_feasibility_obs, processed_feasibility_next_obs
+            processed_feasibility_Qs, processed_feasibility_Vs
 
     def store(self, data_list, additional_dict):
         """
@@ -408,7 +403,7 @@ class RolloutBuffer:
         # store for scenario training
         if self.mode == 'train_scenario':
             scenario_actions, scenario_log_probs, obs, next_obs, rewards, dones, terminated,\
-                feasibility_actions, feasibility_obs, feasibility_next_obs = self.process_CBV_data(data_list, additional_dict)
+                feasibility_Qs, feasibility_Vs = self.process_CBV_data(data_list, additional_dict)
 
             length = len(dones)
             if length > 5:  # remove the too short CBV trajectory
@@ -424,9 +419,8 @@ class RolloutBuffer:
                             self.buffer_dones[self.scenario_pos] = np.array(dones[i])
                             self.buffer_terminated[self.scenario_pos] = np.array(terminated[i])
                             if self.use_feasibility:
-                                self.buffer_feasibility_action[self.scenario_pos, :] = np.array(feasibility_actions[i])
-                                self.buffer_feasibility_obs[self.scenario_pos, :] = np.array(feasibility_obs[i])
-                                self.buffer_feasibility_next_obs[self.scenario_pos, :] = np.array(feasibility_next_obs[i])
+                                self.buffer_feasibility_Qs[self.scenario_pos] = np.array(feasibility_Qs[i])
+                                self.buffer_feasibility_Vs[self.scenario_pos] = np.array(feasibility_Vs[i])
                             self.scenario_pos += 1
                         else:
                             break
@@ -441,9 +435,8 @@ class RolloutBuffer:
                     self.buffer_dones[self.scenario_pos:self.scenario_pos+length] = np.array(dones)
                     self.buffer_terminated[self.scenario_pos:self.scenario_pos+length] = np.array(terminated)
                     if self.use_feasibility:
-                        self.buffer_feasibility_action[self.scenario_pos:self.scenario_pos + length, :] = np.array(feasibility_actions)
-                        self.buffer_feasibility_obs[self.scenario_pos:self.scenario_pos + length, :] = np.array(feasibility_obs)
-                        self.buffer_feasibility_next_obs[self.scenario_pos:self.scenario_pos + length, :] = np.array(feasibility_next_obs)
+                        self.buffer_feasibility_Qs[self.scenario_pos:self.scenario_pos + length] = np.array(feasibility_Qs)
+                        self.buffer_feasibility_Vs[self.scenario_pos:self.scenario_pos + length] = np.array(feasibility_Vs)
                     self.scenario_pos += length
 
             # get the buffer length
@@ -531,9 +524,8 @@ class RolloutBuffer:
             }
             if self.use_feasibility:
                 batch.update({
-                    'feasibility_actions': self.buffer_feasibility_action[:upper_bound, ...],
-                    'feasibility_obs': self.buffer_feasibility_obs[:upper_bound, ...].reshape((-1, self.feasibility_state_dim)),  # obs
-                    'feasibility_next_obs': self.buffer_feasibility_next_obs[:upper_bound, ...].reshape((-1, self.feasibility_state_dim)),  # next obs
+                    'feasibility_Qs': self.buffer_feasibility_Qs[:upper_bound],
+                    'feasibility_Vs': self.buffer_feasibility_Vs[:upper_bound]
                 })
         elif self.mode == 'train_agent':
             index = 0

safebench/scenario/scenario_policy/rl/fppo.py
@@ -54,10 +54,6 @@ class FPPO(BasePolicy):
         self.scenario_id = config['scenario_id']
         self.agent_info = 'Ego_' + config['agent_policy']
 
-        # init the feasibility policy
-        feasibility_config = config['feasibility_config']
-        self.feasibility_policy = FEASIBILITY_LIST[feasibility_config['type']](feasibility_config, logger=logger)
-
         self.policy = CUDA(ActorPPO(dims=self.dims, state_dim=self.state_dim, action_dim=self.action_dim))
         self.policy_optim = torch.optim.Adam(self.policy.parameters(), lr=self.policy_lr, eps=1e-5)  # trick about eps
         self.value = CUDA(CriticPPO(dims=self.dims, state_dim=self.state_dim, action_dim=self.action_dim))
@@ -155,8 +151,8 @@ class FPPO(BasePolicy):
             rewards = CUDA(torch.FloatTensor(batch['rewards']))
             undones = CUDA(torch.FloatTensor(1-batch['dones']))
             unterminated = CUDA(torch.FloatTensor(1-batch['terminated']))
-            feasibility_actions = CUDA(torch.FloatTensor(batch['feasibility_actions']))
-            feasibility_obs = CUDA(torch.FloatTensor(batch['feasibility_obs']))
+            feasibility_Qs = CUDA(torch.FloatTensor(batch['feasibility_Qs']))
+            feasibility_Vs = CUDA(torch.FloatTensor(batch['feasibility_Vs']))
             buffer_size = states.shape[0]
 
             # the values of the reward
@@ -168,8 +164,6 @@ class FPPO(BasePolicy):
             del rewards, undones, values, next_values
 
             # the advantage of the feasibility
-            feasibility_Vs = self.feasibility_policy.get_feasibility_V(feasibility_obs)
-            feasibility_Qs = self.feasibility_policy.get_feasibility_Q(feasibility_obs, feasibility_actions)
             feasibility_advantages = - (feasibility_Qs - feasibility_Vs)
             # condition
             unsafe_condition = torch.where(feasibility_Vs > 0.0, 1.0, 0.0)
@@ -260,8 +254,3 @@ class FPPO(BasePolicy):
         else:
             self.logger.log(f'>> No scenario policy {self.name} model found at {filepath}', 'red')
             self.continue_episode = 0
-
-        # loading the feasibility policy model
-        self.feasibility_policy.load_model(map_name=map_name)
-        self.feasibility_policy.set_mode('eval')
-        assert self.feasibility_policy.continue_episode != 0, 'The scenario policy need well-trained feasibility network'