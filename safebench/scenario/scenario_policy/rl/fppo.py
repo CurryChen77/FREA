9a3b7b2b560c09eae669798092ea618692844d85
currychen <chenkeyu7777@gmail.com>
2024-03-07 18:57:55 +0800

    update fppo

--- safebench/scenario/scenario_policy/rl/fppo.py
+++ safebench/scenario/scenario_policy/rl/fppo.py
@@ -167,13 +167,13 @@ class FPPO(BasePolicy):
             feasibility_advantages = - (feasibility_Qs - feasibility_Vs)
             # condition
             unsafe_condition = torch.where(feasibility_Vs > 0.0, 1.0, 0.0)
-            safe_condition = torch.where(feasibility_Vs <= 0.0, 1.0, 0.0) * torch.where(feasibility_Qs <= 0.0, 1.0, 0.0)
+            safe_condition = torch.where(feasibility_Vs <= 0.0, 1.0, 0.0)
             # multi-advantages
             reward_advantages = (reward_advantages - reward_advantages.mean()) / (reward_advantages.std(dim=0) + 1e-5)
             feasibility_advantages = (feasibility_advantages - feasibility_advantages.mean()) / (feasibility_advantages.std(dim=0) + 1e-5)
             # final advantage
             advantages = unsafe_condition * feasibility_advantages + safe_condition * reward_advantages
-
+            advantages = (advantages - advantages.mean()) / (advantages.std(dim=0) + 1e-5)
             del feasibility_Vs, feasibility_Qs, feasibility_advantages, reward_advantages, unsafe_condition, safe_condition
 
         # start to train, use gradient descent without batch_size
